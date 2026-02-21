6d954f93f1ed0be39fcf195ef00c7eb334d9a7e3
Frija77 <frija456@gmail.com>
2026-02-21 23:27:40 +0700

    Update accessapi.py

--- accessapi.py
+++ accessapi.py
@@ -8,12 +8,13 @@ scope = [
 
 creds = ServiceAccountCredentials.from_json_keyfile_name(
     "elaborate-scope-488115-i1-87b4d3ef1ed9.json", scope
-) #объект для авторицзацит паспорт
+) #объект для авторицзацит паспорт(сюда файл json где будет почта для бота, чтобы подключится к Google sheet)
 
 client = gspread.authorize(creds) #соединение Google Api к Google sheets
 
 sheet = client.open("Schedule").sheet1
 
 data = sheet.get_all_records() #превращает в словари все что есть в самой таблице 
+
 
 #print(data)
